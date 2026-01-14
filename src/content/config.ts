200907edcfbbd50004c0b6bacf01c4bc83b8383f
WhenMoon-afk <84838324+WhenMoon-afk@users.noreply.github.com>
2026-01-14 22:58:19 +0000

    Add src/content/config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,43 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    slug: z.string().optional(),
+    date: z.string(),
+    author: z.string().optional().default('foredark2day'),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().default(false),
+    description: z.string().optional(),
+    image: z.string().optional(),
+  }),
+});
+
+const pages = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    slug: z.string().optional(),
+    date: z.string(),
+    author: z.string().optional().default('foredark2day'),
+    draft: z.boolean().default(false),
+    description: z.string().optional(),
+  }),
+});
+
+const authors = defineCollection({
+  type: 'content',
+  schema: z.object({
+    id: z.string(),
+    name: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string(),
+    bio: z.string().optional(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = { posts, pages, authors };
